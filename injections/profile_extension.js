c19352bc6c719c318279da8345e3def8cb65186b
Brandon Rotkel <brotkel@gmail.com>
2014-10-17 19:41:40 -0700

    Fixed DOM changes in profile page. Profile options now show up on search pages when user is searched for. WinChatty toggle now toggles searches across all buttons.

--- injections/profile_extension.js
+++ injections/profile_extension.js
@@ -4,7 +4,7 @@ var profileExtension = new ProfileExtension();
 function ProfileExtension() {
 	ShacknewsExtension.call(this, "Profile");
 	
-	if (location.pathname.indexOf('/profile/') > -1) {
+	if (location.pathname.indexOf('/profile/') > -1 || location.search.indexOf('chatty_user=') > -1) {
 		this.extendShacknews();
 	}
 }
@@ -16,7 +16,7 @@ function ProfileExtension() {
  *
  */
 ProfileExtension.prototype.getUsername = function() {
-	return $("h3:first").text();
+	return $("input#chatty_user").val();
 }
 
 /**
@@ -26,27 +26,38 @@ ProfileExtension.prototype.getUsername = function() {
  */
 ProfileExtension.prototype.extended = function(eventMessage) {
 	var person = this.getUsername();
-	var lols = document.createElement("a");
-	var enableWinChatty = eventMessage.message;
-	
-	lols.style.color = 'white';
-	lols.innerHTML = ' [<span style="color: orange; font-size: inherit; font-weight: bold; margin: 0; padding: 0 4px;">lol</span>]';
-	lols.href = ShacknewsExtension.LOL.URL + "user.php?authoredby=" + escape(person);
-	lols.target = "_blank";
-
-	$("h3:first").append(lols);
-
-	var status = $('ul.status:first');
-
-	if (enableWinChatty) {
-		$("ul.status li:last").attr("href", "http://winchatty.com/search.php?author=" + escape(person));
+	if (person.length > 0) {
+	  var lols = document.createElement("a");
+  	var enableWinChatty = eventMessage.message;
+  	
+  	lols.innerHTML = ' [<span style="color: orange; font-size: inherit; font-weight: bold; margin: 0; padding: 0 4px;">lol</span>]';
+  	lols.href = ShacknewsExtension.LOL.URL + "user.php?authoredby=" + escape(person);
+  	lols.target = "_blank";
+  
+  	$("ul.search-nav").append("<li></li>");
+  	$("ul.search-nav li:last").append(lols);
+  
+  	var status = $('ul.search-nav:first');
+  
+  	if (enableWinChatty) {
+  		$("ul.search-nav li.on a").attr("href", "http://winchatty.com/nusearch?a=" + escape(person));
+  	}
+  
+  	var search = $("<li></li>");
+  	if (enableWinChatty) {
+    	search.html('<a href="http://winchatty.com/nusearch?q=' + escape(person) + '">Vanity Search</a>');
+  	} else {
+      search.html('<a href="http://www.shacknews.com/search?chatty=1&type=4&chatty_term=' + escape(person) + '&chatty_user=&chatty_author=&chatty_filter=all&result_sort=postdate_desc">Vanity Search</a>');  	
+  	}
+  	status.append(search);
+  
+  	var parent_author = $("<li></li>");
+  	if (enableWinChatty) {
+    	parent_author.html('<a href="http://winchatty.com/nusearch?pa=' + escape(person) + '">Parent Author</a>');
+  	} else {
+    	parent_author.html('<a href="http://www.shacknews.com/search?chatty=1&type=4&chatty_term=&chatty_user=&chatty_author=' + escape(person) + '&chatty_filter=all&result_sort=postdate_desc">Parent Author</a>');
+  	}
+  	
+  	status.append(parent_author);
 	}
-
-	var search = $("<li></li>");
-	search.html('<a href="http://winchatty.com/search.php?terms=' + escape(person) + '">Vanity Search</a>');
-	status.append(search);
-
-	var parent_author = $("<li></li>");
-	parent_author.html('<a href="http://winchatty.com/search.php?parentAuthor=' + escape(person) + '">Parent Author</a>');
-	status.append(parent_author);
 }
